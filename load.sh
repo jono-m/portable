d0c5c283bff39cd8f52059d14e3fcbd08070315c
Jonathan Matthews <jono.jmm@gmail.com>
2015-09-19 01:49:51 -0400

    Added destroy.

--- load.sh
+++ load.sh
@@ -2,13 +2,13 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [-e ~/.bash_profile]; then
-	cp ~/.bash_profile DIR/.temp_bash_profile
+if [ -e ~/.bash_profile ]; then
+	cp ~/.bash_profile $DIR/.temp_bash_profile
 fi
-cp DIR/.bash_profile ~/
+cp $DIR/.bash_profile ~/
 source ~/.bash_profile
 
-if [-e ~/.vimrc]; then
-	cp ~/.vimrc DIR/.temp_vimrc 
+if [ -e ~/.vimrc ]; then
+	cp ~/.vimrc $DIR/.temp_vimrc 
 fi
-cp DIR/.vimrc ~/
+cp $DIR/.vimrc ~/
